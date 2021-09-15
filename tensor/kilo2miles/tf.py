0a6b7eceebdd4ee84a2412963702372523765713
rangapv <rangapv@yahoo.com>
2021-09-15 14:51:52 +0000

    Evaluating the model

--- tensor/kilo2miles/tf.py
+++ tensor/kilo2miles/tf.py
@@ -22,5 +22,15 @@ model.compile(optimizer=tf.keras.optimizers.Adam(1), loss='mean_squared_error')
 print ("Training the model")
 epochs_hist = model.fit(X_train, y_train, epochs = 250)
 
+print("Evaluating the model")
+print(epochs_hist.history.keys())
+
+#graph
+plt.plot(epochs_hist.history['loss'])
+plt.title('Evolution of the error associated with the model')
+plt.xlabel('Epoch')
+plt.ylabel('Training Loss')
+plt.legend('Training Loss')
+
 
 plt.show()
