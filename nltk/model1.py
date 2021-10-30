229d6eb9a1094555c74c52fb2ac1128c84a38abb
rangapv <rangapv@yahoo.com>
2021-10-30 09:42:11 +0000

    Added code to fetch pretrained model fromt he git clone

--- nltk/model1.py
+++ nltk/model1.py
@@ -12,6 +12,9 @@ if not os.path.exists('MODNet'):
 
 
 # dowload the pre-trained ckpt for image matting
-#pretrained_ckpt = 'pretrained/modnet_photographic_portrait_matting.ckpt'
-#if not os.path.exists(pretrained_ckpt):
-#  !gdown --id 1mcr7ALciuAsHCpLnrtG_eop5-EYhbCmz -O pretrained/modnet_photographic_portrait_matting.ckpt 
+pretrained_ckpt = './MODNet/pretrained/modnet_photographic_portrait_matting.ckpt'
+if not os.path.exists(pretrained_ckpt):
+   gd = "gdown --id 1mcr7ALciuAsHCpLnrtG_eop5-EYhbCmz -O pretrained/modnet_photographic_portrait_matting.ckpt" 
+   ps = subprocess.run(gd, capture_output=True, shell=True, text=True, check=False)
+   l22 = ps.stdout
+   print(l22)
