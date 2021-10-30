a8c332a012aaa9ad5eafab57bfe85f5ffcb42a92
rangapv <rangapv@yahoo.com>
2021-10-30 10:22:50 +0000

    Added inference method and training

--- nltk/model1.py
+++ nltk/model1.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 import os
 import subprocess
+import shutil
 
 # clone the repository
 if not os.path.exists('MODNet'):
@@ -18,3 +19,28 @@ if not os.path.exists(pretrained_ckpt):
    ps = subprocess.run(gd, capture_output=True, shell=True, text=True, check=False)
    l22 = ps.stdout
    print(l22)
+
+# clean and rebuild the image folders
+input_folder = './input'
+if os.path.exists(input_folder):
+   print("File with images for background extraction exists")
+   # shutil.rmtree(input_folder)
+else:
+   print("Folder with images DOES NOT Exist") 
+   #os.makedirs(input_folder)
+output_folder = './output'
+if os.path.exists(output_folder):
+  shutil.rmtree(output_folder)
+os.makedirs(output_folder)
+# upload images (PNG or JPG)
+#image_names = list(files.upload().keys())
+#for image_name in image_names:
+#  shutil.move(image_name, os.path.join(input_folder, image_name))
+
+
+app = 'python -m ./MODNet/demo/image_matting/colab/inference --input-path ,/input --output-path ./output --ckpt-path ./pretrained/modnet_photographic_portrait_matting.ckpt'
+pt = subprocess.run(app, capture_output=True, shell=True, text=True, check=False)
+l23 = pt.stdout
+print(pt)
+print(l23)
+
