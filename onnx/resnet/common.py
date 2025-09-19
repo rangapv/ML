45e29fd082a6bf02f805b361cc316b51da1fb4a1
rangapv <rangapv@yahoo.com>
2025-09-19 13:07:51 +0000

    added the resent sample

---
+++ onnx/resnet/common.py
@@ -0,0 +1,174 @@
+
+import argparse
+import os
+
+import tensorflow as tf
+import tensorrt as trt
+import tensorflow.keras as ks
+import ctypes
+#import cuda.cuda as cuda
+import pycuda.driver as cuda
+from cuda import cuda, cudart
+#import pycuda.driver as cuda
+import numpy as np
+from typing import Optional, List, Union
+import os
+import sys
+import urllib
+import urllib.request
+
+
+try:
+    # Sometimes python does not understand FileNotFoundError
+    FileNotFoundError
+except NameError:
+    FileNotFoundError = IOError
+
+
+def GiB(val):
+    return val * 1 << 30
+
+class HostDeviceMem:
+    """Pair of host and device memory, where the host memory is wrapped in a numpy array"""
+    def __init__(self, size: int, dtype: Optional[np.dtype] = None):
+        dtype = dtype or np.dtype(np.uint8)
+        nbytes = size * dtype.itemsize
+        host_mem = (cudart.cudaMallocHost(nbytes))
+        pointer_type = ctypes.POINTER(np.ctypeslib.as_ctypes_type(dtype))
+        print(f'dtype is {dtype}')
+        print(f'nbyres os {nbytes}')
+        print(f'size is {size}')
+        print(f'host_meme in call is {host_mem}')
+        print(f'pointer type is {pointer_type}')
+        #hm1 = ctypes.pointer(host_mem)
+        #print(f'the host mem pointer is {hm1}')
+        ctypescast1 = ctypes.cast(host_mem[1], pointer_type)
+        print(f'ctypecast1 is {ctypescast1}')
+        self._host = np.ctypeslib.as_array(ctypescast1, (size,))
+        #self._host = np.ctypeslib.as_array(ctypes.cast(host_mem, pointer_type), (size,))
+        dv1 = cudart.cudaMalloc(nbytes)
+        self._device = (dv1[1])
+        self._nbytes = nbytes
+
+    @property
+    def host(self) -> np.ndarray:
+        return self._host
+
+    @host.setter
+    def host(self, data: Union[np.ndarray, bytes]):
+        if isinstance(data, np.ndarray):
+            if data.size > self.host.size:
+                raise ValueError(
+                    f"Tried to fit an array of size {data.size} into host memory of size {self.host.size}"
+                )
+            np.copyto(self.host[:data.size], data.flat, casting='safe')
+        else:
+            assert self.host.dtype == np.uint8
+            self.host[:self.nbytes] = np.frombuffer(data, dtype=np.uint8)
+
+    @property
+    def device(self) -> int:
+        return self._device
+
+    @property
+    def nbytes(self) -> int:
+        return self._nbytes
+
+    def __str__(self):
+        return f"Host:\n{self.host}\nDevice:\n{self.device}\nSize:\n{self.nbytes}\n"
+
+    def __repr__(self):
+        return self.__str__()
+
+    def free(self):
+        (cudart.cudaFree(self.device))
+        (cudart.cudaFreeHost(self.host.ctypes.data))
+
+#
+def allocate_buffers(engine: trt.ICudaEngine, profile_idx: Optional[int] = None):
+ inputs = []
+ outputs = []
+ bindings = []
+ stream = cudart.cudaStreamCreate()
+ print(f'stream is {stream}')
+
+ tensor_names = [e1.get_tensor_name(i) for i in range(e1.num_io_tensors)]
+ print(f'tensor name sare {tensor_names}')
+#idx1 = e1.getBidingIndex(tensor_names[0])
+#print(f'the profile-idx is {idx1}')
+ profile_idx = 0
+
+ for binding in tensor_names:
+    bind1 = e1.get_tensor_shape(binding)
+    bind12 = e1.get_tensor_profile_shape(binding, profile_idx)[-1]
+    print(f'bind1 is {bind1}')
+    print(f'bind12 is {bind12}')
+    shape = e1.get_tensor_shape(binding) if profile_idx is None else e1.get_tensor_profile_shape(binding, profile_idx)[-1]
+    shape_valid = np.all([s >= 0 for s in shape])
+    if not shape_valid and profile_idx is None:
+       raise ValueError(f"Binding {binding} has dynamic shape, " +\
+                "but no profile was specified.")
+    size = trt.volume(shape)
+    trt_type = e1.get_tensor_dtype(binding)
+    print(f'tensor is {binding}')
+    print(f'shape is {shape}')
+    print(f'sze is {size}')
+    print(f'trt_type is {trt_type}')
+
+    if trt.nptype(trt_type):
+         dtype = np.dtype(trt.nptype(trt_type))
+         print(f'in f dtype is {dtype}')
+         bindingMemory = HostDeviceMem(size, dtype)
+    else: # no numpy support: create a byte array instead (BF16, FP8, INT4)
+         size = int(size * trt_type.itemsize)
+         bindingMemory = HostDeviceMem(size)
+
+
+    bindings.append(int(bindingMemory.device))
+    # Append to the appropriate list.
+    if e1.get_tensor_mode(binding) == trt.TensorIOMode.INPUT:
+         inputs.append(bindingMemory)
+    else:
+         outputs.append(bindingMemory)
+    #return inputs, outputs, bindings, stream
+    print(f'binding memeory is {bindingMemory} for {binding}')
+    print(f'binding  deviceis {bindingMemory.device}for {binding}')
+
+
+def memcpy_host_to_device(device_ptr: int, host_arr: np.ndarray):
+    nbytes = host_arr.size * host_arr.itemsize
+    cuda_call(cudart.cudaMemcpy(device_ptr, host_arr, nbytes, cudart.cudaMemcpyKind.cudaMemcpyHostToDevice))
+
+    # Wrapper for cudaMemcpy which infers copy size and does error checking
+def memcpy_device_to_host(host_arr: np.ndarray, device_ptr: int):
+    nbytes = host_arr.size * host_arr.itemsize
+    cuda_call(cudart.cudaMemcpy(host_arr, device_ptr, nbytes, cudart.cudaMemcpyKind.cudaMemcpyDeviceToHost))
+
+
+
+
+def _do_inference_base(inputs, outputs, stream, execute_async_func):
+    # Transfer input data to the GPU.
+    kind = cudart.cudaMemcpyKind.cudaMemcpyHostToDevice
+    [cuda_call(cudart.cudaMemcpyAsync(inp.device, inp.host, inp.nbytes, kind, stream)) for inp in inputs]
+    # Run inference.
+    execute_async_func()
+    # Transfer predictions back from the GPU.
+    kind = cudart.cudaMemcpyKind.cudaMemcpyDeviceToHost
+    [cuda_call(cudart.cudaMemcpyAsync(out.host, out.device, out.nbytes, kind, stream)) for out in outputs]
+    # Synchronize the stream
+    cuda_call(cudart.cudaStreamSynchronize(stream))
+    # Return only the host outputs.
+    return [out.host for out in outputs]
+
+
+# This function is generalized for multiple inputs/outputs.
+# inputs and outputs are expected to be lists of HostDeviceMem objects.
+def do_inference(context, engine, bindings, inputs, outputs, stream):
+    def execute_async_func():
+        context.execute_async_v3(stream_handle=stream)
+    # Setup context tensor address.
+    num_io = engine.num_io_tensors
+    for i in range(num_io):
+        context.set_tensor_address(engine.get_tensor_name(i), bindings[i])
+    return _do_inference_base(inputs, outputs, stream, execute_async_func)
---
+++ onnx/resnet/new1.py
@@ -0,0 +1,137 @@
+#!/usr/bin/env python3
+#author: rangapv@yahoo.com
+#19-09-25
+
+
+
+import os
+
+# This sample uses an ONNX ResNet50 Model to create a TensorRT Inference Engine
+import random
+import sys
+import tensorflow as tf
+import tensorrt as trt
+import tensorflow.keras as ks
+import ctypes
+#import cuda.cuda as cuda
+import pycuda.driver as cuda
+from cuda import cuda, cudart
+#import pycuda.driver as cuda
+import common
+import numpy as np
+from typing import Optional, List, Union
+import os
+import sys
+import urllib
+import urllib.request
+
+
+class ModelData(object):
+    MODEL_PATH = "ResNet50.onnx"
+    INPUT_SHAPE = (3, 224, 224)
+    # We can convert TensorRT data types to numpy types with trt.nptype()
+    DTYPE = trt.float32
+
+
+# You can set the logger severity higher to suppress messages (or lower to display more messages).
+TRT_LOGGER = trt.Logger(trt.Logger.WARNING)
+
+
+def build_engine_onnx(model_file):
+    builder = trt.Builder(TRT_LOGGER)
+    network = builder.create_network(0)
+    config = builder.create_builder_config()
+    parser = trt.OnnxParser(network, TRT_LOGGER)
+
+    config.set_memory_pool_limit(trt.MemoryPoolType.WORKSPACE, common.GiB(1))
+    # Load the Onnx model and parse it in order to populate the TensorRT network.
+    with open(model_file, "rb") as model:
+        if not parser.parse(model.read()):
+            print("ERROR: Failed to parse the ONNX file.")
+            for error in range(parser.num_errors):
+                print(parser.get_error(error))
+            return None
+
+    engine_bytes = builder.build_serialized_network(network, config)
+    runtime = trt.Runtime(TRT_LOGGER)
+    return runtime.deserialize_cuda_engine(engine_bytes)
+
+
+
+def load_normalized_test_case(test_image, pagelocked_buffer):
+    # Converts the input image to a CHW Numpy array
+    def normalize_image(image):
+        # Resize, antialias and transpose the image to CHW.
+        c, h, w = ModelData.INPUT_SHAPE
+        image_arr = (
+            np.asarray(image.resize((w, h), Image.LANCZOS))
+            .transpose([2, 0, 1])
+            .astype(trt.nptype(ModelData.DTYPE))
+            .ravel()
+        )
+        # This particular ResNet50 model requires some preprocessing, specifically, mean normalization.
+        return (image_arr / 255.0 - 0.45) / 0.225
+
+    # Normalize the image and copy to pagelocked memory.
+    np.copyto(pagelocked_buffer, normalize_image(Image.open(test_image)))
+    return test_image
+
+
+def main():
+    # Set the data path to the directory that contains the trained models and test images for inference.
+    # Get test images, models and labels.
+    test_images = ["./beach.jpg","./car.JPEG"]
+    onnx_model_file = "./resnet50.onnx"
+
+    #labels
+    label_url = 'https://storage.googleapis.com/download.tensorflow.org/data/ImageNetLabels.txt'
+    class_labels = urllib.request.urlopen(label_url).read().splitlines()
+    class_labels = class_labels[1:] # remove the first class which is background
+    assert len(class_labels) == 1000
+
+    # make sure entries of class_labels are strings
+    for i, label in enumerate(class_labels):
+      if isinstance(label, bytes):
+         class_labels[i] = label.decode("utf8")
+
+    #labels = open(class_labels, "r").read().split("\n")
+
+    labels_file = "/usr/src/tensorrt/data/resnet50/class_labels.txt" 
+    labels = open(labels_file, "r").read().split("\n")
+
+    # Build a TensorRT engine.
+    engine = build_engine_onnx(onnx_model_file)
+    # Inference is the same regardless of which parser is used to build the engine, since the model architecture is the same.
+    # Allocate buffers and create a CUDA stream.
+    inputs, outputs, bindings, stream = common.allocate_buffers(engine)
+    # Contexts are used to perform inference.
+    context = engine.create_execution_context()
+
+    # Load a normalized test case into the host input page-locked buffer.
+    test_image = random.choice(test_images)
+    test_case = load_normalized_test_case(test_image, inputs[0].host)
+    # Run the engine. The output will be a 1D tensor of length 1000, where each value represents the
+    # probability that the image corresponds to that label
+    trt_outputs = common.do_inference(
+        context,
+        engine=engine,
+        bindings=bindings,
+        inputs=inputs,
+        outputs=outputs,
+        stream=stream,
+    )
+
+
+
+
+# We use the highest probability as our prediction. Its index corresponds to the predicted label.
+    pred = labels[np.argmax(trt_outputs[0])]
+    common.free_buffers(inputs, outputs, stream)
+    if "_".join(pred.split()) in os.path.splitext(os.path.basename(test_case))[0]:
+        print("Correctly recognized " + test_case + " as " + pred)
+    else:
+        print("Incorrectly recognized " + test_case + " as " + pred)
+
+if __name__ == "__main__":
+    main()
+
