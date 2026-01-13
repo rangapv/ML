010dcd7b65f45e45dad9bf2343a0fec981a77df4
rangapv <rangapv@yahoo.com>
2026-01-13 10:59:50 +0000

    support for single lora

---
+++ tensorRT_LLM/LoRA/s1.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python3
+#author:rangapv@yahoo.com
+#30-12-25
+
+
+from tensorrt_llm import LLM
+from tensorrt_llm.lora_manager import LoraConfig
+from tensorrt_llm.executor.request import LoRARequest
+from tensorrt_llm.sampling_params import SamplingParams
+
+#base_model="meta-llama/llama-7b-hf/"
+#base_model="TinyLlama/TinyLlama-1.1B-Chat-v1.0"
+base_model="meta-llama/Llama-2-7b-hf"
+##meta-llama/Llama-2-7b-hf
+#lora_weights="kunishou/Japanese-Alpaca-LoRA-7b-v0"
+#kunishou/Japanese-Alpaca-LoRA-7b-v0
+
+
+# Configure LoRA
+lora_config = LoraConfig(
+    lora_dir=["./lora_imdb"],
+    max_lora_rank=8,
+    max_loras=1,
+    max_cpu_loras=1
+)
+
+# Initialize LLM with LoRA support
+llm = LLM(
+    model=base_model,
+    lora_config=lora_config
+)
+
+# Create LoRA request
+lora_request = LoRARequest("my-lora-task", 0, "./lora_imdb")
+
+# Generate with LoRA
+prompts = ["Once upon a time"]
+sampling_params = SamplingParams(max_tokens=50)
+
+outputs = llm.generate(
+    prompts,
+    sampling_params,
+    lora_request=[lora_request]
+)
+
+print(outputs[0])
