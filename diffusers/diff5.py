de5e4b5f9b8b2a7f54513aebef9f75c70e2e2f6f
rangapv <rangapv@yahoo.com>
2026-01-21 11:00:59 +0000

    trying out inpainting

---
+++ diffusers/diff5.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python3
+#author:rangapv@yahoo.com
+#20-01-26
+
+from io import BytesIO
+import PIL
+import torch
+from diffusers import DiffusionPipeline, StableDiffusionInpaintPipeline
+
+import gc
+
+def clear_memory():
+    """Clear GPU memory"""
+    gc.collect()
+    torch.cuda.empty_cache()
+
+
+clear_memory()
+torch.no_grad()
+torch.cuda.empty_cache()
+print(torch.cuda.memory_summary(device=None, abbreviated=False))
+
+def download_image(url):
+    response = url
+    return PIL.Image.open(response).convert("RGB")
+
+
+
+
+init_image=download_image("./result4.png").resize((512, 512))
+mask_image=download_image("./result5.png").resize((512, 512))
+
+pipe = StableDiffusionInpaintPipeline.from_pretrained(
+    "stable-diffusion-v1-5/stable-diffusion-inpainting", torch_dtype=torch.float16
+)
+pipe = pipe.to("cuda")
+
+prompt = """
+Convert the background to skyblue
+highly detailed, high budget hollywood movie, cinemascope, moody, epic, gorgeous, film grain
+"""
+image = pipe(prompt=prompt, image=init_image, mask_image=mask_image).images[0]
+image.save("result52.png")
