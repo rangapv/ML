a4df8bb25f18be6b2f0400b732c7b13e6eac0e6e
rangapv <rangapv@yahoo.com>
2026-03-05 13:33:56 +0000

    3D rendering of Images

---
+++ tile/3DGS/nerfstudio/install-nvdiffrast.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+#author:rangapv@yahoo.com
+#05-03-2026
+
+nvd1=`pip3 install git+https://github.com/NVlabs/nvdiffrast.git@253ac4fcea7de5f396371124af597e6cc957bfae --no-build-isolation`
