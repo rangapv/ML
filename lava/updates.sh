a2df375d8399d0a2eb0244ef17104a2b725a23af
rangapv <rangapv@yahoo.com>
2026-05-10 11:12:08 +0000

    Script to run to get lava scripts for training and updating Pytorch

---
+++ lava/updates.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+#author:rangapv@yahoo.com
+#10-05-26
+
+#Reference: https://github.com/haotian-liu/LLaVA
+
+fix1(){
+
+lavatmp="lava-temp1"
+cm1=`mkdir $lavatmp`
+cm2=`cd $lavatmp;git init;git clone https://github.com/haotian-liu/LLaVA.git`
+cm3=`pip3 install --upgrade torch --index-url https://download.pytorch.org/whl/nightly/cu132`
+cm4=`pip3 install --upgrade pip`
+cm5=`pip3 install -e .`
+cm6=`pip3 install -e ".[train]"`
+cm7=`pip3 install flash-attn --no-build-isolation`
+
+
+}
+
+fix1
