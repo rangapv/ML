2021f91d14d39f5012af3faa75acde15cbb53603
rangapv <rangapv@yahoo.com>
2026-05-14 03:01:43 +0000

    Updated script to build wheel

--- lava/updates.sh
+++ lava/updates.sh
@@ -8,13 +8,15 @@ fix1(){
 
 lavatmp="lava-temp1"
 cm1=`mkdir $lavatmp`
+cm11=`mkdir $lavatmp/wheels`
 cm2=`cd $lavatmp;git init;git clone https://github.com/haotian-liu/LLaVA.git`
 cm4=`cd $lavatmp/LLaVA;pip3 install --upgrade pip`
 cm5=`cd $lavatmp/LLaVA;pip3 install -e .`
 cm6=`cd $lavatmp/LLaVA;pip3 install -e ".[train]"`
 cm7=`pip3 install --upgrade torch==2.11.0`
 cm8=`pip3 install --upgrade torch --index-url https://download.pytorch.org/whl/nightly/cu132`
-cm7=`cd $lavatmp/LLaVA;MAX_JOBS=4 pip3 install flash-attn --no-build-isolation --no-cache-dir`
+cm9=`cd $lavatmp/LLaVA;MAX_JOBS=4 pip3 wheel flash-attn -w ../wheels/ --no-build-isolation`
+#cm7=`cd $lavatmp/LLaVA;MAX_JOBS=4 pip3 install flash-attn --no-build-isolation --no-cache-dir`
 
 }
 
