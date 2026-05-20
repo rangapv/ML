a2cf7addacb2f602713588216afda81f58d28708
rangapv <rangapv@yahoo.com>
2026-05-20 13:27:09 +0000

    Updated pip install script with Model downloads

--- lava/flshpip.sh
+++ lava/flshpip.sh
@@ -16,3 +16,22 @@ cm70=`curl -s https://s3.us-east-2.amazonaws.com/flash-attn-2.8.3/flash_attn-2.8
 #cm73=`aws s3 cp s3://flash-attn-2.8.3/flash_attn-2.8.3-cp310-cp310-linux_x86_64.whl`
 cm71=`pip3 install ./flash_attn-2.8.3-cp310-cp310-linux_x86_64.whl`
 
+
+chkpt_dwn(){
+
+chk0=`pip3 install --upgrade huggingface_hub`
+chk1=`hf auth login --no-add-to-git-credential`
+
+env1=`export HF_HUB_DISABLE_SYMLINKS=1`
+env2=`export HF_HUB_DISABLE_SYMLINKS_WARNING=True`
+chk2=`hf download meta-llama/Meta-Llama-3-8B-Instruct --local-dir ./$lavatmp/LLaVA/checkpoints/Meta-Llama-3-8B-Instruct`
+
+chk3=`hf download openai/clip-vit-large-patch14-336 --local-dir ./$lavatmp/LLaVA/checkpoints/clip-vit-large-patch14-336`
+
+chk4=`hf download liuhaotian/llava-v1.5-mlp2x-336px-pretrain-vicuna-7b-v1.5 --local-dir ./$lavatmp/LLaVA/checkpoints/llava-pretrain-projector`
+
+chk5=`hf download lmsys/vicuna-7b-v1.5 --local-dir ./checkpoints/vicuna-7b-v1.5 --local-dir ./$lavatmp/LLaVA/checkpoints/ `
+
+}
+
+chkpt_dwn
