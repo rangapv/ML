c084ab6ad9799cea503189a1d50a28936a51d928
rangapv <rangapv@yahoo.com>
2026-08-14 08:13:16 +0000

    More modular

--- tile/3DGS/source_install.sh
+++ tile/3DGS/source_install.sh
@@ -10,32 +10,44 @@ wi2=`pip3 install --user --upgrade packaging setuptools`
 
 wi3=`pip3 install tyro`
 
-si1=`sudo apt install unzip`
-
-si2=`pip3 uninstall -y tensorflow keras tf-keras tensorflow-io-gcs-filesystem`
-
 wi4=`wget https://s3.us-east-2.amazonaws.com/gsplat-1.5.3/gsplat-1.5.3-cp310-cp310-linux_x86_64.whl`
 
 wi5=`pip3 install ./gsplat-1.5.3-cp310-cp310-linux_x86_64.whl`
-
-
-#TEST:
-
-python3 -c "import gsplat; print(gsplat.__file__); from gsplat import color_correct; print('ok')"
 
 
 
 wi5=`pip3 install nerfview`
 
 
-si3=`mkdir sample1;cd sample1;git init;git pull https://github.com/nerfstudio-project/gsplat.git`
-
-si4=`cd ./sample1/examples; python3 datasets/download_dataset.py`
-
-si5=`cd ./sample1/examples;bash benchmarks/basic.sh`
-
-
 }
 
 
+system_ins() {
+
+
+si1=`sudo apt install unzip`
+
+si2=`pip3 uninstall -y tensorflow keras tf-keras tensorflow-io-gcs-filesystem`
+
+si3=`mkdir ~/sample1;cd ~/sample1;git init;git pull https://github.com/nerfstudio-project/gsplat.git;git submodule update --init --recursive`
+
+#TEST:
+
+si4=`python3 -c "import gsplat; print(gsplat.__file__); from gsplat import color_correct; print('ok')"`
+si4s="$?"
+
+if ( "$si4s" == "0" )
+then
+si5=`cd ~/sample1/examples; python3 datasets/download_dataset.py`
+si5s="$?"
+si6p=`sed -i "s|python|python3|g" ~/sample1/examples/benchmarks/basic.sh`
+si5=`cd ~/sample1/examples;bash benchmarks/basic.sh`
+else
+	echo "gsplat missing utlities libraary..hint do a recursive PUll and try again"
+fi
+
+}
+
 wheel_install
+
+system_ins
