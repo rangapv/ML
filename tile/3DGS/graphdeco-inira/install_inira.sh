055e7df95031ecafa21cc88bd5533dc40e5582aa
rangapv <rangapv@yahoo.com>
2026-09-08 11:13:57 +0000

    Updated sanity check before the graphdeco-inira install

--- tile/3DGS/graphdeco-inira/install_inira.sh
+++ tile/3DGS/graphdeco-inira/install_inira.sh
@@ -30,13 +30,55 @@ si5=`cd gaussian-splatting;python3 train.py -s ./tandt/train`
 
 build_check(){
 
+cmd1=("$@")
+tcmd="$#"
+insdep=0
 
+echo "the total depedency to check is $tcmd"
 
+for i in "${cmd1[@]}"
+do
 
+wc=`which $i`
+wcs="$?"
 
+if [[ ( $wcs == "0" ) ]]
+then
+	v1=`($i -V | awk -v pk1="$i" '{split($2,a,"."); if (a[1] == "3") { if (a[2] == "10") print pk1 (" verison satisfies gapghdeco-inira Install") } }')`
+    echo "$v1"
+    echo "\"$i\" is installed @ $wc and its version is `$i -V` proceeding with other checks"
+else
+    echo "\"$i\"  is not installed .pls install it and then re-run this script for other tasks"
+    insdep=1
+fi
+
+done
+
+if (( $insdep == 1 ))
+then
+   echo "Install all the dependencies and proceed after, exiting now"
+   exit
+else
+   echo "All the dependecy \" ${cmd1[@]} \" are installed"
+fi
 
 }
 
+check_gcc (){
+i1="gcc"
+i11=`which $i1`
+i11s="$?"
+if [[ ( $i11s == "0" ) ]]
+then
+#`gcc --version | grep gcc |awk '{split($0,a," "); print a[NF]}'`
+v2=`gcc --version | grep gcc | awk -v pk2="$i1" '{split($3,a,"."); if (a[1] == "11") { print pk2 (" version satisifies graphdeco-inira install") }}'`
+echo "$v2"
+else
+	echo "$i1 is not the required version for graphdeco-inira"
+fi
+}
 
 
+build_check python3
+check_gcc
 install_inira
